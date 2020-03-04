816bae2af10fb52bcf6523aca4ca5bbb171a70d9
Cameron Bristol <cbristol28@yahoo.com>
2020-03-04 09:23:16 -0600

    Moved everything to the driver class for testing purposes, will reassess later

--- Classes/MortgageCalculator.php
+++ Classes/MortgageCalculator.php
@@ -1,24 +1,2 @@
 <?php
 error_reporting(E_ALL);
-
-class MortgageCalculator {
-  private $principle;
-  private $annualInterestRate;
-  private $noMonthlyPayments;
-
-  function __construct(){
-    $principle = 0;
-    $annualInterestRate = 0;
-    $noMonthlyPayments = 0;
-  }
-
-  function __construct1($newPrinc, $newAIR, $newNMP){
-    $principle = $newPrinc;
-    $annualInterestRate = $newAIR;
-    $noMonthlyPayments = $newNMP;
-  }
-
-  function getPrinciple(){
-    echo $principle;
-  }
-}
--- driver.php
+++ driver.php
@@ -1,8 +1,25 @@
 <?php
   error_reporting(E_ALL);
-  include 'Classes';
-  echo "Testing";
-  echo "here";
-  $testing = new MortgageCalculator();
-  echo "here";
-  echo "The value is " . $testing->getPrinciple();
+
+  class MortgageCalculator {
+    public $principle = 0;
+    public $annualInterestRate = 0;
+    public $noMonthlyPayments = 0;
+
+    public function __construct(){
+      $a = func_get_args();
+      $i = func_num_args();
+      if (method_exists($this,$f='__construct'.$i)) {
+          call_user_func_array(array($this,$f),$a);
+      }
+    }
+
+    public function __construct3($newPrinc, $newAIR, $newNMP){
+      $this->principle = $newPrinc;
+      $this->annualInterestRate = $newAIR;
+      $this->noMonthlyPayments = $newNMP;
+    }
+  }
+  echo "Testing ";
+  $testing = new MortgageCalculator(27000,5,6);
+  echo "The value is " . $testing->principle;
